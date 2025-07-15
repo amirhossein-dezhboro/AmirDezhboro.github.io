6025037ec8498a1e0b56de03fafa6bc5e5342337
AmirHossein Dezhboro <a.dejboro@gmail.com>
2025-07-15 18:12:00 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const fadeElems = document.querySelectorAll('.fade-in');
+
+    const appearOptions = {
+        threshold: 0.1,
+        rootMargin: "0px 0px -50px 0px"
+    };
+
+    const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                entry.target.style.opacity = '1';
+                entry.target.style.transform = 'translateY(0)';
+                appearOnScroll.unobserve(entry.target);
+            }
+        });
+    }, appearOptions);
+
+    fadeElems.forEach(elem => {
+        appearOnScroll.observe(elem);
+    });
+});
